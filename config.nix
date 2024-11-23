d36bffb17c0f458e00b850215bd23ccfb62f11b1
Morgs <morganc.vandyk@gmail.com>
2024-11-23 14:30:11 +0200

    added some home manager stuff

---
+++ config.nix
@@ -0,0 +1,85 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ 
+  config, 
+  pkgs,
+  host,
+  username,
+  options,
+  ... 
+}:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "morgan-desktop"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Africa/Johannesburg";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_ZA.UTF-8";
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "za";
+    variant = "";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.morgs = {
+    isNormalUser = true;
+    description = "Morgan";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  
+  #Flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes"];
+
+  programs.hyprland.enable = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    brave
+    git
+    gh
+    kitty
+    neovim
+    wget
+  ];
+
+  # Services
+  # RootKit
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
---
+++ home.nix
@@ -0,0 +1,85 @@
+{ 
+   pkgs,
+   username,
+   host,
+   ... }:
+{
+  # This needs to actually be set to your username
+  home.username = "${username}";
+  home.homeDirectory = "/home/${username}";
+  home.stateVersion = "24.05";
+     
+  # Import Program Configs
+  imports = [
+    #./neovim.nix
+  ];
+
+  # Install & Configure Git
+  programs.git = {
+    enable = true;
+    userName = "Morgs";
+    userEmail = "morganc.vandyk@gmail.com";
+  };
+
+  # Create XDG Dirs
+  xdg = {
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+    };
+  };
+  gtk = {
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+  };
+  qt = {
+    enable = true;
+    style.name = "adwaita-dark";
+    platformTheme.name = "gtk3";
+  };
+
+  # Programs
+  programs = {
+    gh.enable = true;
+    btop = {
+     enable = true;
+     settings = {
+       vim_keys = true;
+     };
+    };
+    kitty = {
+     enable = true;
+     package = pkgs.kitty;
+     settings = {
+       scrollback_lines = 2000;
+       wheel_scroll_min_lines = 1;
+       window_padding_width = 4;
+       confirm_os_window_close = 0;
+     };
+     extraConfig = ''
+       tab_bar_style fade
+       tab_fade 1
+       active_tab_font_style   bold
+       inactive_tab_font_style bold
+     '';
+    };
+    bash = {
+      enable = true;
+      enableCompletion = true;
+      shellAliases = {
+        svim = "sudo nvim";
+	vim = "nvim";
+	ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
+      };
+    };
+  };
+  #home-manager.enable = true;
+}
