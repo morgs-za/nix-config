eed808db89b752442133a3f581949adabd122af5
zaMorgs <morganc.vandyk@gmail.com>
2025-01-24 20:21:05 +0200

    last update

--- system/amd-drivers.nix
+++ system/amd-drivers.nix
@@ -14,7 +14,7 @@ in
   };
 
   config = mkIf cfg.enable {
-    systemd.tmpfiles.rules = [ "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}" ];
+  #systemd.tmpfiles.rules = [ "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}" ];
     services.xserver.videoDrivers = [ "amdgpu" ];
   };
 }
