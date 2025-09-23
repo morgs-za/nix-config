78592c5bb88247be89c2bfaf12fc5f61e6fe394e
Morgs <morganc.vandyk@gmail.com>
2025-09-23 22:10:47 +0200

    Enable PipeWire and add pavucontrol for audio management

--- system/packages.nix
+++ system/packages.nix
@@ -7,6 +7,15 @@
     xdg-user-dirs
   ];
 
+  # Enable PipeWire for audio
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;  # PulseAudio compatibility
+    jack.enable = true;   # JACK compatibility
+  };
+
   nix = {
     package = pkgs.nixVersions.stable;
     extraOptions = ''
